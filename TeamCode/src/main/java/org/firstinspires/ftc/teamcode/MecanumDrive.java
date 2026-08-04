5d4883749b43dc8efb918ae81ff960aebde3b0df
novarae126 <nova.rae126@gmail.com>
2026-08-04 19:52:31 -0400

    Created Repository and Made a Mecanum Drive Class for the mecanum pushbots

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
@@ -0,0 +1,54 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+@TeleOp
+public class MecanumDrive extends OpMode {
+    private double x_command, y_command, z_command;
+    private double frontLeftCommand, frontRightCommand, backLeftCommand, backRightCommand;
+    private DcMotor frontLeft, frontRight, backLeft, backRight;
+
+
+    @Override
+    public void init() {
+        frontLeft = hardwareMap.get(DcMotor.class, "frontLeft");
+        frontRight = hardwareMap.get(DcMotor.class, "frontRight");
+        backLeft = hardwareMap.get(DcMotor.class, "backLeft");
+        backRight = hardwareMap.get(DcMotor.class, "backRight");
+        frontRight.setDirection(DcMotorSimple.Direction.REVERSE);
+        backRight.setDirection(DcMotorSimple.Direction.REVERSE);
+    }
+
+    @Override
+    public void loop() {
+
+        x_command = gamepad1.left_stick_x;
+        y_command = -gamepad1.left_stick_y;
+        z_command = gamepad1.right_stick_x;
+
+        frontLeftCommand = y_command + x_command - z_command;
+        frontRightCommand = y_command - x_command + z_command;
+        backLeftCommand = y_command - x_command - z_command;
+        backRightCommand = y_command + x_command + z_command;
+
+        double max = Math.max(1, frontLeftCommand);
+        max = Math.max(max, frontRightCommand);
+        max = Math.max(max, backLeftCommand);
+        max = Math.max(max, backRightCommand);
+
+        frontRightCommand = frontRightCommand/max;
+        frontLeftCommand = frontLeftCommand/max;
+        backRightCommand = backRightCommand/max;
+        backLeftCommand = backLeftCommand/max;
+
+        frontLeft.setPower(frontLeftCommand);
+        frontRight.setPower(frontRightCommand);
+        backLeft.setPower(backLeftCommand);
+        backRight.setPower(backRightCommand);
+
+
+    }
+}
