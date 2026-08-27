2c7f54be774009eb99165d51207ddd6c79191317
SkibidiohioNoah <noahkalis61@gmail.com>
2026-08-27 18:48:36 -0400

    67 ohio

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
@@ -21,8 +21,8 @@ public class Constants {
             .leftRearMotorDirection(DcMotorSimple.Direction.FORWARD)
             .rightFrontMotorDirection(DcMotorSimple.Direction.REVERSE)
             .rightRearMotorDirection(DcMotorSimple.Direction.REVERSE);
-    public static FollowerConstants followerConstants = new FollowerConstants()
-    .mass(5.125);
+//    public static FollowerConstants followerConstants = new FollowerConstants().mass(5.125); <- is for the push bot
+public static FollowerConstants followerConstants = new FollowerConstants().mass(13.4263342);
     public static PathConstraints pathConstraints = new PathConstraints(0.99, 100, 1, 1);
 
     public static Follower createFollower(HardwareMap hardwareMap) {
