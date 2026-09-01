b02b0bd26ce2af503c3f2f1c4bf588e81e8b9ca8
SkibidiohioNoah <noahkalis61@gmail.com>
2026-09-01 18:47:57 -0400

    67 ohio

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testAuton.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testAuton.java
@@ -1,13 +1,18 @@
-package org.firstinspires.ftc.teamcode.pedroPathing;
+package org.firstinspires.ftc.teamcode;
 
+package org.firstinspires.ftc.teamcode.pedroPathing; // make sure this aligns with class location
 import com.pedropathing.follower.Follower;
 import com.pedropathing.geometry.BezierCurve;
 import com.pedropathing.geometry.BezierLine;
 import com.pedropathing.geometry.Pose;
+import com.pedropathing.ivy.Command;
+import com.pedropathing.ivy.Scheduler;
 import com.pedropathing.paths.PathChain;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.robocol.Command;
+import static com.pedropathing.ivy.Scheduler.*;
+import static com.pedropathing.ivy.pedro.PedroCommands.*;
+import static com.pedropathing.ivy.groups.Groups.*;
 
 @Autonomous
 public class testAuton extends LinearOpMode {
